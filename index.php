6976156e8be13fc35ad13c5f1374ce536853f060
Thomas <82337499+Tomazku@users.noreply.github.com>
2024-04-11 19:34:25 +0200

    Changements des fichiers en php et ajout du fichier header.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Découvrez le zoo Arcadia</title>
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    
+</body>
+</html>
