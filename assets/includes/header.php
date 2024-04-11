6eaee9cb1851ea43eeb6fe408ed6bec6873de7e5
Thomas <82337499+Tomazku@users.noreply.github.com>
2024-04-11 19:50:45 +0200

    PRoblème de fichier en local

---
+++ assets/includes/header.php
@@ -0,0 +1,36 @@
+<head>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+    <header>
+        <div class="logo"></div>
+        <nav>
+            <ul class="menu">
+                <li>Menu 1
+                    <ul>
+                        <li>Sous-menu 1</li>
+                        <li>Sous-menu 2</li>
+                        <li>Sous-menu 3</li>
+                    </ul>
+                </li>
+                <li>Menu 2
+                    <ul>
+                        <li>Sous-menu 4</li>
+                        <li>Sous-menu 5</li>
+                        <li>Sous-menu 6</li>
+                    </ul>
+                </li>
+                <li>Menu 3
+                    <ul>
+                        <li>Sous-menu 7</li>
+                        <li>Sous-menu 8</li>
+                        <li>Sous-menu 9</li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <a href="#" class="contact-button">Contact</a>
+    </header>
+</body>
+</html>
