9288ed8191c6f831a672f778911f8399a9c1a2d9
Thomas <82337499+Tomazku@users.noreply.github.com>
2024-04-12 18:35:54 +0200

    Ajout du logo et du favicon + léger changement dans le header

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -2,30 +2,27 @@
     <!-- <link rel="stylesheet" href="../css/main.css"> -->
 </head>
     <header>
-        <div class="logo"></div>
+        <div class="logo">
+            <img src="/assets/images/logo.png" alt=""><a href="./header.php"></a>
+        </div>
         <nav>
             <ul class="menu">
-                <li>Menu 1
+                <li>ACCEUIL</li>
+                <li>SERVICES
                     <ul>
                         <li>Sous-menu 1</li>
                         <li>Sous-menu 2</li>
                         <li>Sous-menu 3</li>
                     </ul>
                 </li>
-                <li>Menu 2
+                <li>HABITATS
                     <ul>
                         <li>Sous-menu 4</li>
                         <li>Sous-menu 5</li>
                         <li>Sous-menu 6</li>
                     </ul>
                 </li>
-                <li>Menu 3
-                    <ul>
-                        <li>Sous-menu 7</li>
-                        <li>Sous-menu 8</li>
-                        <li>Sous-menu 9</li>
-                    </ul>
-                </li>
+                <li>CONTACTS</li>
             </ul>
         </nav>
         <a href="#" class="contact-button">Contact</a>
