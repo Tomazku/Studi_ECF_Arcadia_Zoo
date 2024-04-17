d3c01e89a7074bb8deb8a50a08345ec380131d42
Thomas <82337499+Tomazku@users.noreply.github.com>
2024-04-17 21:25:50 +0200

    Finition de la section des habitats et début du grid pour la section des animaux

--- index.php
+++ index.php
@@ -26,12 +26,54 @@
     <p>Explorez une variété d'habitats captivants, de la jungle exotique à la vaste savane et aux mystérieux marais, lors de votre visite au Zoo Arcadia.</p>
     <button class='button'>Nos habitats</button>
   </div>
-</div>
-    
+  </div>
+  
+<div class="card-container">
+  <div class="card">
+      <img src="assets/images/Jungle.jpg" alt="">
+    <div class="card-content">
+      <p class="text-carte">
+      Plongez au cœur de la luxuriante jungle tropicale au Zoo Arcadia, où les arbres majestueux et la végétation dense abritent une multitude de créatures exotiques, des singes espiègles aux oiseaux aux couleurs vives.
+      </p>
+      <button class="button">Découvrez la jungle</button>
+    </div>
+  </div>
 
+  <div class="card">
+    <div class="card-content">
+      <p class="text-carte">
+      Dans la vaste savane, vous serez émerveillé par la vue des éléphants majestueux errant paisiblement parmi les hautes herbes, tandis que les lions paresseux se prélassent sous le soleil chaud.      </p>
+      <button class="button">Découvrez la savane</button>
+    </div>
+      <img src="assets/images/Savane.jpg" alt="">
+  </div>
 
+  <div class="card">
+      <img src="assets/images/Marais.jpg" alt="">
+    <div class="card-content">
+      <p class="text-carte">
+      Plongez au cœur de la luxuriante jungle tropicale au Zoo Arcadia, où les arbres majestueux et la végétation dense abritent une multitude de créatures exotiques, des singes espiègles aux oiseaux aux couleurs vives.      </p>
+      <button class="button">Découvrez les marais</button>
+    </div>
+  </div>
+</div>    
 
-    <?php include 'assets/includes/footer.php'; ?>
+<!-- section animaux -->
+</section>
+
+<section class="animaux">
+  <div class="container-animaux">
+  <H2>Découvrez nos <span class="orange-text">Animaux</span></H2>
+        <div class="grid-autruche ">
+          <img class="large-img" src="assets/images/Autruche.jpg" alt="">
+          <h4 class="title-animaux">Autruche</h4>
+        </div>
+  </div>
+    </section>
+
+   
+
+<?php include 'assets/includes/footer.php'; ?>
 <script src="/assets/scripts/script.js"></script>
 </body>
 </html>
