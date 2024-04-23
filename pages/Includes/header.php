8a14b3859ba9a967ebe2f31d28c0c964abf26e0f
Thomas <82337499+Tomazku@users.noreply.github.com>
2024-04-23 17:56:49 +0200

    Récupération du header

---
+++ pages/Includes/header.php
@@ -0,0 +1,44 @@
+<header class='container-header'>
+    <!-- Logo -->
+    <div class="header">
+        <div class='logo'>
+            <a href="index.php">
+                <img src="assets/images/logo.png" alt="Logo du zoo Arcadia">
+            </a>
+        </div>
+        <div class='menu'>
+            
+            <nav>
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li>
+                        <a href="services.php">Services</a>
+                        <ul>
+                            <li><a href="services.php#restauration">
+                                <img src="assets/images/Restaurant.jpg" alt="Restauration">Restauration</a></li>
+                            <li><a href="services.php#animations">
+                                <img src="assets/images/Zoo_tour.jpg" alt="Animations">Animations</a></li>
+                            <li><a href="services.php#activites">
+                                <img src="assets/images/petit_train.jpg" alt="Activités">Activités</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="habitats.php">Habitats</a>
+                        <ul>
+                            <li><a href="habitats.php#savane">
+                                <img src="assets/images/Savane.jpg" alt="Savane">Savane</a></li>
+                            <li><a href="habitats.php#jungle">
+                                <img src="assets/images/Jungle.jpg" alt="Jungle">Jungle</a></li>
+                            <li><a href="habitats.php#marais">
+                                <img src="assets/images/Marais.jpg" alt="Marais">Marais</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+            <div class="connexion">
+                <button onclick="window.location.href=''">Connexion</button>
+            </div>
+    </div>
+</header>
