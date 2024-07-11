382dbf3eaa141552f2885a8b67e05e3693338c35
Thomas <82337499+Tomazku@users.noreply.github.com>
2024-07-11 20:58:36 +0200

    Fin de l'effet de scroll

--- hamburger.js
+++ hamburger.js
@@ -1,12 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector('.hamburger');
     const menu = document.querySelector('.menu');
+    const header = document.querySelector('.container-header');
+    let lastScrollTop = 0;
+    let initialTop = getComputedStyle(header).top;
 
     hamburger.addEventListener('click', function() {
         menu.classList.toggle('active');
         hamburger.classList.toggle('active'); 
+    });
 
+    window.addEventListener('scroll', function() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            // Scroll vers le bas
+            header.style.top = `-${header.offsetHeight}px`; // Cache le header
+        } else {
+            // Scroll vers le haut
+            header.style.top = initialTop; // Remet le header à sa position initiale
+        }
+        
+        lastScrollTop = scrollTop;
     });
 });
-
-
