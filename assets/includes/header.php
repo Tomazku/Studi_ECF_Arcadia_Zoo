50ee2aaa0d62aca656153503b3483b6285ce9bad
Thomas <82337499+Tomazku@users.noreply.github.com>
2024-07-11 20:26:09 +0200

    Test sur le header - travail sur une animation au clic du menu hambuger + position fixe au scroll

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -6,28 +6,26 @@
                 <img src="assets/images/logo.png" alt="Logo du zoo Arcadia">
             </a>
         </div>
-        <!-- <div class="hamburger">&#9776;</div> Hamburger icon -->
+        <!--<div class="hamburger">&#9776;</div>--> <!-- Hamburger icon -->
         <div class="hamburger" onclick="toggleHamburger(this)">
             <div class="bar1"></div>
             <div class="bar2"></div>
             <div class="bar3"></div>
         </div>
         <div class='menu'>
+            
             <nav>
                 <ul>
                     <li><a href="index.php">Accueil</a></li>
-                    <li><a href="services.php">Services</a></li>
+                    <li><a href="services.php">Services</a> </li>
                     <li><a href="habitats.php">Habitats</a></li>
                     <li><a href="horaires.php">Horaires</a></li>
                     <li><a href="contact.php">Contact</a></li>
                 </ul>
             </nav>
         </div>
-        <div class="connexion">
+            <div class="connexion">
             <button class="button-link" onclick="window.open('./pages/Back-end/login.php', '_blank')">Connexion</button>
-        </div>
+            </div>
     </div>
-
 </header>
-
-
--- hamburger.js
+++ hamburger.js
@@ -1,20 +1,12 @@
-    // document.addEventListener('DOMContentLoaded', function() {
-    //         const hamburger = document.querySelector('.hamburger');
-    //         const menu = document.querySelector('.menu');
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.menu');
 
-    //         hamburger.addEventListener('click', function() {
-    //             menu.classList.toggle('active');
-    //         });
-    //     });
+    hamburger.addEventListener('click', function() {
+        menu.classList.toggle('active');
+        hamburger.classList.toggle('active'); 
 
-
-    document.addEventListener('DOMContentLoaded', function() {
-        const hamburger = document.querySelector('.hamburger');
-        const menu = document.querySelector('.menu');
-    
-        hamburger.addEventListener('click', function() {
-            menu.classList.toggle('active');
-            hamburger.classList.toggle('active'); 
-        });
     });
-    
+});
+
+
